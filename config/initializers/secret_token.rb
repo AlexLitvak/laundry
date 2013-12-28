27e386f1604ce25908fb2b279706f322e04fda32
AlexFiverr <alex.l@fiverr.com>
2013-12-28 13:17:03 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laundry::Application.config.secret_token = 'e4527e8c9479965b70071bfc483a7a4560cc6bd7bbf2f0dbaabef0989a455e1435716c1c739e00534f2e0e64aa625f86249bc0436145406d3ad0d4507257b439'
